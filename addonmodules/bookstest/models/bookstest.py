27f17d6a79686f8135e62dc2ae97101c9581e45c
Martin Peterson <Martin.Peterson@ucsmindbite.se>
2023-12-20 08:56:29 +0100

    testmodule

---
+++ addonmodules/bookstest/models/bookstest.py
@@ -0,0 +1,9 @@
+from odoo import models, fields
+
+class LibraryBook(models.Model):
+    _name = 'library.book'
+    _description = 'Library Book'
+
+    name = fields.Char(string='Title', required=True)
+    author = fields.Char(string='Author')
+    isbn = fields.Char(string='ISBN')
